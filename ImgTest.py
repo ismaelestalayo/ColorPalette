6dd067d47ca557f910b456ba6f1bf766e78414cc
Ismael <ismael.em@outlook.com>
2017-02-08 23:33:17 +0100

    First commit

---
+++ ImgTest.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy.misc as smp
+from PIL import Image
+
+im = Image.open("Skyline.jpg")
+pix = im.load()
+
+for x in range(0, im.width):
+    for y in range(0, im.height):
+        pix[x, y] = pix[x,y][0], 0, 0
+
+im.save("TESTT.jpg")
+print("> File saved.")
+
+img = smp.toimage(im)
+img.show()
