1e0a4baa3e1083ca7f98f770582b256b70d69155
Ismael <ismael.em@outlook.com>
2017-02-10 01:01:54 +0100

    Commit desde

--- ImgTest.py
+++ ImgTest.py
@@ -5,12 +5,12 @@ from PIL import Image
 im = Image.open("Skyline.jpg")
 pix = im.load()
 
-for x in range(0, im.width):
-    for y in range(0, im.height):
-        pix[x, y] = pix[x,y][0], 0, 0
+#    for y in range(0, im.height):
+#for x in range(0, im.width):
+#        pix[x, y] = pix[x,y][0], 0, 0
 
-im.save("TESTT.jpg")
-print("> File saved.")
+#im.save("TESTT.jpg")
+#print("> File saved.")
 
 img = smp.toimage(im)
 img.show()
